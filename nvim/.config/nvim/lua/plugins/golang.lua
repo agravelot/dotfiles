60915ae0c732d1db61f4ff1093c40b54231c3c15
Antoine GRAVELOT <antoine.gravelot@hotmail.fr>
2024-04-22 10:24:35 +0200

    add ray x go

--- nvim/.config/nvim/lua/plugins/golang.lua
+++ nvim/.config/nvim/lua/plugins/golang.lua
@@ -25,4 +25,33 @@ return {
       })
     end,
   },
+  {
+    "ray-x/go.nvim",
+    dependencies = { -- optional packages
+      "ray-x/guihua.lua",
+      "neovim/nvim-lspconfig",
+      "nvim-treesitter/nvim-treesitter",
+    },
+    config = function()
+      require("go").setup({
+        gofmt = "gofumpt", -- gofmt through gopls: alternative is gofumpt, goimports, golines, gofmt, etc
+        trouble = true,
+        luasnip = true,
+      })
+    end,
+    event = { "CmdlineEnter" },
+    ft = { "go", "gomod" },
+    build = ':lua require("go.install").update_all_sync()', -- if you need to install/update all binaries
+    keys = {
+      { "<leader>cta", "<cmd>GoAddTag<cr>", desc = "Add Tag" },
+      { "<leader>cta", "<cmd>GoRmTag<cr>", desc = "Remove Tag" },
+      { "<leader>cR", "<cmd>GoRename<cr>", desc = "Rename 2" },
+      { "<leader>cfs", "<cmd>GoFillStruct<cr>", desc = "Fill Struct" },
+      { "<leader>cfS", "<cmd>GoFillSwitch<cr>", desc = "Fill Switch" },
+      { "<leader>cv", "<cmd>GoVet<cr>", desc = "Run Go Vet" },
+      { "<leader>cL", "<cmd>GoLint<cr>", desc = "Run Linter" },
+      { "<leader>cD", "<cmd>GoDoc<cr>", desc = "Show Documentation" },
+      { "<leader>ci", "<cmd>GoImpl<cr>", desc = "Implement" },
+    },
+  },
 }
