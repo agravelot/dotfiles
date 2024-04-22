cbae8dd446c269811d28bdf5cdea11f416c49767
Antoine GRAVELOT <antoine.gravelot@hotmail.fr>
2024-04-22 10:22:43 +0200

    fix: double ts lsp

--- nvim/.config/nvim/lua/plugins/typescript.lua
+++ nvim/.config/nvim/lua/plugins/typescript.lua
@@ -141,51 +141,51 @@ return {
     },
   },
   -- keymap code actions
-  {
-    "neovim/nvim-lspconfig",
-    opts = {
-      -- make sure mason installs the server
-      servers = {
-        ---@type lspconfig.options.tsserver
-        tsserver = {
-          keys = {
-            {
-              "<leader>co",
-              function()
-                vim.lsp.buf.code_action({
-                  apply = true,
-                  context = {
-                    only = { "source.organizeImports.ts" },
-                    diagnostics = {},
-                  },
-                })
-              end,
-              desc = "Organize Imports",
-            },
-            {
-              "<leader>cR",
-              function()
-                vim.lsp.buf.code_action({
-                  apply = true,
-                  context = {
-                    only = { "source.removeUnused.ts" },
-                    diagnostics = {},
-                  },
-                })
-              end,
-              desc = "Remove Unused Imports",
-            },
-          },
-          ---@diagnostic disable-next-line: missing-fields
-          settings = {
-            completions = {
-              completeFunctionCalls = true,
-            },
-          },
-        },
-      },
-    },
-  },
+  -- {
+  --   "neovim/nvim-lspconfig",
+  --   opts = {
+  --     -- make sure mason installs the server
+  --     servers = {
+  --       ---@type lspconfig.options.tsserver
+  --       tsserver = {
+  --         keys = {
+  --           {
+  --             "<leader>co",
+  --             function()
+  --               vim.lsp.buf.code_action({
+  --                 apply = true,
+  --                 context = {
+  --                   only = { "source.organizeImports.ts" },
+  --                   diagnostics = {},
+  --                 },
+  --               })
+  --             end,
+  --             desc = "Organize Imports",
+  --           },
+  --           {
+  --             "<leader>cR",
+  --             function()
+  --               vim.lsp.buf.code_action({
+  --                 apply = true,
+  --                 context = {
+  --                   only = { "source.removeUnused.ts" },
+  --                   diagnostics = {},
+  --                 },
+  --               })
+  --             end,
+  --             desc = "Remove Unused Imports",
+  --           },
+  --         },
+  --         ---@diagnostic disable-next-line: missing-fields
+  --         settings = {
+  --           completions = {
+  --             completeFunctionCalls = true,
+  --           },
+  --         },
+  --       },
+  --     },
+  --   },
+  -- },
   {
     "mfussenegger/nvim-dap",
     optional = true,
