a66e0522ed8f66367f56882f91c8fd88077daae6
Antoine GRAVELOT <antoine.gravelot@hotmail.fr>
2024-05-24 15:53:59 +0200

    do not conflict refactor

--- nvim/.config/nvim/lua/plugins/refactor.lua
+++ nvim/.config/nvim/lua/plugins/refactor.lua
@@ -21,37 +21,37 @@
 
 -- load refactoring Telescope extension
 
-vim.keymap.set({ "n", "x" }, "<leader>rr", function()
-  require("telescope").extensions.refactoring.refactors()
-end)
-
+-- vim.keymap.set({ "n", "x" }, "<leader>rr", function()
+--   require("telescope").extensions.refactoring.refactors()
+-- end)
+--
 return {
-  "ThePrimeagen/refactoring.nvim",
-  dependencies = {
-    "nvim-lua/plenary.nvim",
-    "nvim-treesitter/nvim-treesitter",
-  },
-  config = function(_, opts)
-    require("refactoring").setup(opts or {})
-    require("telescope").load_extension("refactoring")
-  end,
-  keys = {
-    { "x", "<leader>re", "<cmd>Refactor extract<cr>", desc = "Extract" },
-    { "x", "<leader>rf", "<cmd>Refactor extract_to_file<cr>", desc = "Extract to File" },
-    { "x", "<leader>rv", "<cmd>Refactor extract_var<cr>", desc = "Extract to Variable" },
-    { "x", "<leader>ri", "<cmd>Refactor inline_var<cr>", desc = "Inline variable" },
-    { "x", "<leader>rI", "<cmd>Refactor inline_func<cr>", desc = "Inline Function" },
-    { "x", "<leader>rb", "<cmd>Refactor extract_block<cr>", desc = "Extract Block" },
-    { "x", "<leader>rB", "<cmd>Refactor extract_block_to_file<cr>", desc = "Extract Block to File" },
-    { "x", "<leader>rr", "<cmd>require('refactoring').select_refactor()<cr>", desc = "Select Refactor" },
-  },
-  {
-    "folke/which-key.nvim",
-    optional = true,
-    opts = {
-      defaults = {
-        ["<leader>r"] = { name = "+refactor" },
-      },
-    },
-  },
+  --   "ThePrimeagen/refactoring.nvim",
+  --   dependencies = {
+  --     "nvim-lua/plenary.nvim",
+  --     "nvim-treesitter/nvim-treesitter",
+  --   },
+  --   config = function(_, opts)
+  --     require("refactoring").setup(opts or {})
+  --     require("telescope").load_extension("refactoring")
+  --   end,
+  --   keys = {
+  --     { "x", "<leader>re", "<cmd>Refactor extract<cr>", desc = "Extract" },
+  --     { "x", "<leader>rf", "<cmd>Refactor extract_to_file<cr>", desc = "Extract to File" },
+  --     { "x", "<leader>rv", "<cmd>Refactor extract_var<cr>", desc = "Extract to Variable" },
+  --     { "x", "<leader>ri", "<cmd>Refactor inline_var<cr>", desc = "Inline variable" },
+  --     { "x", "<leader>rI", "<cmd>Refactor inline_func<cr>", desc = "Inline Function" },
+  --     { "x", "<leader>rb", "<cmd>Refactor extract_block<cr>", desc = "Extract Block" },
+  --     { "x", "<leader>rB", "<cmd>Refactor extract_block_to_file<cr>", desc = "Extract Block to File" },
+  --     { "x", "<leader>rr", "<cmd>require('refactoring').select_refactor()<cr>", desc = "Select Refactor" },
+  --   },
+  --   {
+  --     "folke/which-key.nvim",
+  --     optional = true,
+  --     opts = {
+  --       defaults = {
+  --         ["<leader>r"] = { name = "+refactor" },
+  --       },
+  --     },
+  --   },
 }
