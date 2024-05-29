1d98d10cdc6715be760b40596137c4af075c12d0
Antoine GRAVELOT <antoine.gravelot@hotmail.fr>
2024-05-30 00:04:40 +0200

    nvim: add smart open

--- nvim/.config/nvim/lua/plugins/custom.lua
+++ nvim/.config/nvim/lua/plugins/custom.lua
@@ -258,4 +258,37 @@ return {
   --     }
   --   end,
   -- },
+  -- {
+  --   "m4xshen/hardtime.nvim",
+  --   dependencies = { "MunifTanjim/nui.nvim", "nvim-lua/plenary.nvim" },
+  --   opts = {},
+  -- },
+
+  {
+    "danielfalk/smart-open.nvim",
+    branch = "0.2.x",
+    config = function()
+      require("telescope").load_extension("smart_open")
+    end,
+    opts = {
+      show_scores = true,
+    },
+    keys = {
+      {
+        "<leader><leader>",
+        -- function()
+        --   require("telescope").extensions.smart_open.smart_open()
+        -- end,
+        "<cmd>Telescope smart_open<cr>",
+        desc = "Smart Open",
+      },
+    },
+    dependencies = {
+      "kkharji/sqlite.lua",
+      -- Only required if using match_algorithm fzf
+      { "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
+      -- Optional.  If installed, native fzy will be used when match_algorithm is fzy
+      { "nvim-telescope/telescope-fzy-native.nvim" },
+    },
+  },
 }
