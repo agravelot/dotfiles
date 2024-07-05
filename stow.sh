d7bcef1d7acf6210850bc61718bf2600f546d116
Antoine GRAVELOT <antoine.gravelot@hotmail.fr>
2024-07-05 21:19:26 +0200

    add lazygit in stow

--- stow.sh
+++ stow.sh
@@ -1,8 +1,8 @@
-#!/usr/env/bin bash
+#!/usr/bin/env bash
 # stow.sh
 # This script is used to stow all the dotfiles in the current directory
 
-MODULES="kitty fish git nvim tmux yabai alacritty karabiner yabai skhd starship zsh tmuxinator"
+MODULES="kitty fish git nvim tmux yabai alacritty karabiner yabai skhd starship zsh tmuxinator lazygit"
 
 for module in $MODULES; do
 	echo "Stowing $module"
