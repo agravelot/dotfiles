ff605ae18123e71215fa058f24c18cc390c7e5b9
Antoine GRAVELOT <antoine.gravelot@hotmail.fr>
2024-07-06 00:07:39 +0200

    nvim: lazyload md plugin

--- nvim/.config/nvim/lua/plugins/custom.lua
+++ nvim/.config/nvim/lua/plugins/custom.lua
@@ -500,9 +500,8 @@ return {
   {
     "OXY2DEV/markview.nvim",
 
+    ft = { "markdown", "mdx" },
     dependencies = {
-      -- You may not need this if you don't lazy load
-      -- Or if the parsers are in your $RUNTIMEPATH
       "nvim-treesitter/nvim-treesitter",
 
       "nvim-tree/nvim-web-devicons",
