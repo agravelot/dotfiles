1e1b87b2e6653618f56d79b21be543abf980235a
Antoine GRAVELOT <antoine.gravelot@hotmail.fr>
2024-07-06 02:23:19 +0200

    nvim: add docker

--- nvim/.config/nvim/lua/plugins/docker.lua
+++ nvim/.config/nvim/lua/plugins/docker.lua
@@ -38,4 +38,108 @@ return {
   --     },
   --   },
   -- },
+  {
+    "lpoto/telescope-docker.nvim",
+    dependencies = {
+      "nvim-telescope/telescope.nvim",
+    },
+    config = function()
+      require("telescope").load_extension("docker")
+    end,
+    -- :Telescope docker containers
+    -- :Telescope docker images
+    -- :Telescope docker networks
+    -- :Telescope docker volumes
+    -- :Telescope docker contexts
+    -- :Telescope docker swarm
+    -- :Telescope docker machines
+    -- :Telescope docker compose
+    -- :Telescope docker files
+    keys = {
+      { "<leader>DD", "<cmd>Telescope docker<cr>", desc = "[D]ocker" },
+      {
+        "<leader>Dc",
+        "<cmd>Telescope docker containers<cr>",
+        desc = "Docker [c]ontainers",
+      },
+      {
+        "<leader>DC",
+        "<cmd>Telescope docker compose<cr>",
+        desc = "Docker [C]ompose",
+      },
+      {
+        "<leader>Di",
+        "<cmd>Telescope docker images<cr>",
+        desc = "Docker [i]mages",
+      },
+      {
+        "<leader>Dn",
+        "<cmd>Telescope docker networks<cr>",
+        desc = "Docker [n]etworks",
+      },
+      {
+        "<leader>Dv",
+        "<cmd>Telescope docker volumes<cr>",
+        desc = "Docker [v]olumes",
+      },
+      {
+        "<leader>Df",
+        "<cmd>Telescope docker files<cr>",
+        desc = "Docker [f]iles",
+      },
+      {
+        "<leader>Ds",
+        "<cmd>Telescope docker swarm<cr>",
+        desc = "Docker [s]warm",
+      },
+      {
+        "<leader>Dm",
+        "<cmd>Telescope docker machines<cr>",
+        desc = "Docker [m]achines",
+      },
+      {
+        "<leader>Dx",
+        "<cmd>Telescope docker contexts<cr>",
+        desc = "Docker conte[x]ts",
+      },
+    },
+  },
+  {
+    "folke/which-key.nvim",
+    optional = true,
+    opts = {
+      defaults = {
+        ["<leader>D"] = { name = "+Docker" },
+      },
+    },
+  },
+  {
+    "piersolenski/telescope-import.nvim",
+    dependencies = "nvim-telescope/telescope.nvim",
+    config = function()
+      require("telescope").setup({
+        extensions = {
+          import = {
+            insert_at_top = false,
+            custom_languages = {
+              -- Golang
+              {
+                regex = [[^\t(\".*\")|^import (\".*\")]],
+                filetypes = { "go" },
+                extensions = { "go" },
+              },
+            },
+          },
+        },
+      })
+      require("telescope").load_extension("import")
+    end,
+    keys = {
+      {
+        "<leader>ci",
+        "<cmd>Telescope import<cr>",
+        desc = "Telescope [i]mport",
+      },
+    },
+  },
 }
