a944853f1795ec0dccdb97a4fc254af481172c2b
Antoine GRAVELOT <antoine.gravelot@hotmail.fr>
2024-11-06 09:37:10 +0100

    migrate to lspconfi buf

--- nvim/.config/nvim/lua/plugins/buf.lua
+++ nvim/.config/nvim/lua/plugins/buf.lua
@@ -6,7 +6,7 @@ return {
     opts = function(_, opts)
       opts.ensure_installed = opts.ensure_installed or {}
       vim.list_extend(opts.ensure_installed, {
-        "bufls",
+        "buf",
         "pbls",
       })
     end,
