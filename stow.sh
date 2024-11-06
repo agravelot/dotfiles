94c23904060da52f573e55c1c7d08b4dbabcfebe
Antoine GRAVELOT <antoine.gravelot@hotmail.fr>
2024-11-06 09:40:10 +0100

    ssh config for work

--- stow.sh
+++ stow.sh
@@ -2,7 +2,7 @@
 # stow.sh
 # This script is used to stow all the dotfiles in the current directory
 
-MODULES="kitty fish git nvim tmux yabai alacritty karabiner yabai skhd starship zsh tmuxinator lazygit"
+MODULES="kitty fish git nvim tmux yabai alacritty karabiner yabai skhd starship zsh tmuxinator lazygit ssh"
 
 for module in $MODULES; do
 	echo "Stowing $module"
