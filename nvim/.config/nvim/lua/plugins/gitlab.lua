e4360324574e70810b4d7fe57b2744e5035e24a6
Antoine GRAVELOT <antoine.gravelot@hotmail.fr>
2024-12-18 11:47:21 +0100

    disable gitlab things

--- nvim/.config/nvim/lua/plugins/gitlab.lua
+++ nvim/.config/nvim/lua/plugins/gitlab.lua
@@ -8,32 +8,32 @@ return {
   --     },
   --   },
   -- },
-  {
-    "williamboman/mason-lspconfig",
-    dependencies = { "neovim/nvim-lspconfig", "williamboman/mason.nvim" },
-    ---@class PluginLspOpts
-    opts = function(_, opts)
-      opts.ensure_installed = opts.ensure_installed or {}
-      vim.list_extend(opts.ensure_installed, {
-        "gitlab_ci_ls",
-      })
-    end,
-  },
-  {
-    "harrisoncramer/gitlab.nvim",
-    dependencies = {
-      "MunifTanjim/nui.nvim",
-      "nvim-lua/plenary.nvim",
-      "sindrets/diffview.nvim",
-      "stevearc/dressing.nvim", -- Recommended but not required. Better UI for pickers.
-      "nvim-tree/nvim-web-devicons", -- Recommended but not required. Icons in discussion tree.
-    },
-    enabled = true,
-    build = function()
-      require("gitlab.server").build(true)
-    end, -- Builds the Go binary
-    config = function()
-      require("gitlab").setup()
-    end,
-  },
+  -- {
+  --   "williamboman/mason-lspconfig",
+  --   dependencies = { "neovim/nvim-lspconfig", "williamboman/mason.nvim" },
+  --   ---@class PluginLspOpts
+  --   opts = function(_, opts)
+  --     opts.ensure_installed = opts.ensure_installed or {}
+  --     vim.list_extend(opts.ensure_installed, {
+  --       "gitlab_ci_ls",
+  --     })
+  --   end,
+  -- },
+  -- {
+  --   "harrisoncramer/gitlab.nvim",
+  --   dependencies = {
+  --     "MunifTanjim/nui.nvim",
+  --     "nvim-lua/plenary.nvim",
+  --     "sindrets/diffview.nvim",
+  --     "stevearc/dressing.nvim", -- Recommended but not required. Better UI for pickers.
+  --     "nvim-tree/nvim-web-devicons", -- Recommended but not required. Icons in discussion tree.
+  --   },
+  --   enabled = true,
+  --   build = function()
+  --     require("gitlab.server").build(true)
+  --   end, -- Builds the Go binary
+  --   config = function()
+  --     require("gitlab").setup()
+  --   end,
+  -- },
 }
