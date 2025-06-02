4cd51673ad08254f5d7a093188522eb38aff4c85
Antoine GRAVELOT <antoine.gravelot@hotmail.fr>
2025-06-03 01:34:16 +0200

    nvim: disable scrollbar, never used

--- nvim/.config/nvim/lua/plugins/scrollbar.lua
+++ nvim/.config/nvim/lua/plugins/scrollbar.lua
@@ -1,22 +1,22 @@
 return {
-  { -- scrollbar with information
-    "lewis6991/satellite.nvim",
-    event = "VeryLazy",
-    init = function()
-      vim.api.nvim_create_autocmd({ "ColorScheme", "VimEnter" }, {
-        desc = "User: Define `SatelliteQuickfix` hlgroup",
-        callback = function()
-          vim.api.nvim_set_hl(0, "SatelliteQuickfix", { link = "DiagnosticSignInfo" })
-        end,
-      })
-    end,
-    opts = {
-      winblend = 10, -- little transparency, since hard to see in many themes otherwise
-      handlers = {
-        cursor = { enable = false },
-        marks = { enable = false }, -- prevents buggy mark mappings
-        quickfix = { enable = true },
-      },
-    },
-  },
+  -- { -- scrollbar with information
+  --   "lewis6991/satellite.nvim",
+  --   event = "VeryLazy",
+  --   init = function()
+  --     vim.api.nvim_create_autocmd({ "ColorScheme", "VimEnter" }, {
+  --       desc = "User: Define `SatelliteQuickfix` hlgroup",
+  --       callback = function()
+  --         vim.api.nvim_set_hl(0, "SatelliteQuickfix", { link = "DiagnosticSignInfo" })
+  --       end,
+  --     })
+  --   end,
+  --   opts = {
+  --     winblend = 10, -- little transparency, since hard to see in many themes otherwise
+  --     handlers = {
+  --       cursor = { enable = false },
+  --       marks = { enable = false }, -- prevents buggy mark mappings
+  --       quickfix = { enable = true },
+  --     },
+  --   },
+  -- },
 }
