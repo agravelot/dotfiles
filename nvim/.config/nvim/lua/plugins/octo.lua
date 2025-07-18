470a02e4ab6a437dcafe2a559b538522fcea4c72
Antoine GRAVELOT <antoine.gravelot@hotmail.fr>
2025-07-18 10:29:52 +0200

    nvim: add octo keymap

--- nvim/.config/nvim/lua/plugins/octo.lua
+++ nvim/.config/nvim/lua/plugins/octo.lua
@@ -12,5 +12,12 @@ return {
         ssh_aliases = { ["work.github.com"] = "github.com" }, -- SSH aliases. e.g. `ssh_aliases = {["github.com-work"] = "github.com"}`. The key part will be interpreted as an anchored Lua pattern.
       })
     end,
+    keys = {
+      { "<leader>gu", "<cmd>Octo<cr>", desc = "Open Octo" },
+      { "<leader>guh", "<cmd>Octo pr list<cr>", desc = "Open PR list" },
+      { "<leader>guf", "<cmd>Octo issue list<cr>", desc = "Open Issue list" },
+      { "<leader>gup", "<cmd>Octo pr create<cr>", desc = "Create PR" },
+      { "<leader>gui", "<cmd>Octo issue create<cr>", desc = "Create Issue" },
+    },
   },
 }
