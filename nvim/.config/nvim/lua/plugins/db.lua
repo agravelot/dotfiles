f61b06bfa198087b3c3d58f84b625735ee1700c7
Antoine GRAVELOT <antoine.gravelot@hotmail.fr>
2025-07-18 10:32:30 +0200

    nvim: add sqlruff

--- nvim/.config/nvim/lua/plugins/db.lua
+++ nvim/.config/nvim/lua/plugins/db.lua
@@ -1,19 +1,30 @@
 return {
   {
-    "kristijanhusak/vim-dadbod-ui",
-    dependencies = {
-      { "tpope/vim-dadbod", lazy = true },
-      { "kristijanhusak/vim-dadbod-completion", ft = { "sql", "mysql", "plsql" }, lazy = true },
-    },
-    cmd = {
-      "DBUI",
-      "DBUIToggle",
-      "DBUIAddConnection",
-      "DBUIFindBuffer",
-    },
-    init = function()
-      -- Your DBUI configuration
-      vim.g.db_ui_use_nerd_fonts = 1
+    "stevearc/conform.nvim",
+    optional = true,
+    opts = function(_, opts)
+      opts.formatters.sqruff = {
+        -- args = { "format", "--dialect=ansi", "-" },
+      }
+
+      local sql_ft = { "sql", "mysql", "plsql" }
+      for _, ft in ipairs(sql_ft) do
+        opts.formatters_by_ft[ft] = opts.formatters_by_ft[ft] or {}
+        table.insert(opts.formatters_by_ft[ft], "sqruff")
+        -- remove sqlfluff
+        opts.formatters_by_ft[ft] = vim.tbl_filter(function(name)
+          return name ~= "sqlfluff"
+        end, opts.formatters_by_ft[ft])
+      end
+    end,
+  },
+  {
+    "mason-org/mason.nvim",
+    opts = function(_, opts)
+      opts.ensure_installed = opts.ensure_installed or {}
+      vim.list_extend(opts.ensure_installed, {
+        "sqruff",
+      })
     end,
   },
 }
