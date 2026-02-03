d194e221a6cdc5f335750c59944155013efbc165
Antoine GRAVELOT <antoine.gravelot@hotmail.fr>
2026-02-03 02:48:51 +0100

    remove karabiner

--- stow.sh
+++ stow.sh
@@ -2,9 +2,11 @@
 # stow.sh
 # This script is used to stow all the dotfiles in the current directory
 
-MODULES="kitty fish git nvim tmux yabai alacritty karabiner yabai skhd starship zsh tmuxinator lazygit ssh hyprland"
+MODULES="kitty fish git nvim tmux yabai alacritty yabai skhd starship zsh tmuxinator lazygit ssh hyprland"
 
 for module in $MODULES; do
 	echo "Stowing $module"
 	stow -t ~ "$module"
 done
+
+# stow -t ~ "$MODULES"
