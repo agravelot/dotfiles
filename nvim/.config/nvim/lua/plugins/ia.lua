9e66727534d32ce7296c59d2dc4dcb5463c7d000
Antoine GRAVELOT <antoine.gravelot@hotmail.fr>
2026-02-03 02:48:38 +0100

    bypass volta shims for copilot

--- nvim/.config/nvim/lua/plugins/ia.lua
+++ nvim/.config/nvim/lua/plugins/ia.lua
@@ -1,3 +1,20 @@
+local function find_node_path()
+  local paths = {
+    "/opt/homebrew/Cellar/node/25.2.1/bin/node",
+    "/opt/homebrew/bin/node",
+    "/usr/local/bin/node",
+    "/usr/bin/node",
+  }
+
+  for _, path in ipairs(paths) do
+    if vim.loop.fs_stat(path) then
+      return path
+    end
+  end
+
+  return "node"
+end
+
 return {
   {
     "olimorris/codecompanion.nvim",
@@ -10,7 +27,7 @@ return {
   {
     "zbirenbaum/copilot.lua",
     opts = {
-      copilot_node_command = "/opt/homebrew/Cellar/node/25.2.1/bin/node",
+      copilot_node_command = find_node_path(), -- solution bypass volta shims
     },
   },
 }
