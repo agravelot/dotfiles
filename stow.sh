66752a3f49061697a7a9cfb8a6979e467b734ad4
Antoine GRAVELOT <antoine.gravelot@hotmail.fr>
2026-03-05 01:00:03 +0100

    add pipwire and opencode

--- stow.sh
+++ stow.sh
@@ -2,11 +2,11 @@
 # stow.sh
 # This script is used to stow all the dotfiles in the current directory
 
-MODULES="kitty fish git nvim tmux yabai alacritty yabai skhd starship zsh tmuxinator lazygit ssh hyprland lab"
+MODULES="kitty fish git nvim tmux yabai alacritty yabai skhd starship zsh tmuxinator lazygit ssh hyprland lab opencode pipewire"
 
 for module in $MODULES; do
-	echo "Stowing $module"
-	stow -t ~ "$module"
+  echo "Stowing $module"
+  stow -t ~ "$module"
 done
 
 # stow -t ~ "$MODULES"
