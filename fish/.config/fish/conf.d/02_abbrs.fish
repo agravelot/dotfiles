c9fc0ea059f65d37d4daecd6b2ed7b25278bdd36
Antoine GRAVELOT <antoine.gravelot@hotmail.fr>
2026-07-09 00:35:23 +0200

    update fish

--- fish/.config/fish/conf.d/02_abbrs.fish
+++ fish/.config/fish/conf.d/02_abbrs.fish
@@ -14,23 +14,51 @@ if test "$TERM" = xterm-kitty
 end
 
 abbr --add g git
-abbr --add gco 'git checkout'
-abbr --add gcb 'git checkout -b'
-abbr --add gst 'git status'
-abbr --add gcm 'git commit -m'
-abbr --add gca 'git commit --amend'
-abbr --add gpl 'git pull'
-abbr --add gps 'git push'
-abbr --add gdf 'git diff'
-abbr --add gaa 'git add -A'
-abbr --add glg 'git log --oneline --graph --all'
+# abbr --add gco 'git checkout'
+# abbr --add gcb 'git checkout -b'
+# abbr --add gst 'git status'
+# abbr --add gcm 'git commit -m'
+# abbr --add gca 'git commit --amend'
+# abbr --add gpl 'git pull'
+# abbr --add gps 'git push'
+# abbr --add gdf 'git diff'
+# abbr --add gaa 'git add -A'
+# abbr --add glg 'git log --oneline --graph --all'
+
+abbr --command=git c "commit -am"
+abbr --command=git tc "commit -am"
+abbr --command=git cm "commit --no-all -m"
+abbr --command=git co checkout
+abbr --command=git s status
+abbr --command=git ts status
+abbr --command=git amend "commit --amend --all --no-edit"
+abbr --command=git hreset "reset --hard"
+abbr --command=git cp cherry-pick
+abbr --command=git cherrypick cherry-pick
+abbr --command=git dif diff
 
 abbr --add d docker
 abbr --add k kubectl
 abbr --add l 'cd ~/lab'
 
-abbr --add cat 'bat --theme=ainsi'
+abbr --add cat 'bat -P'
 abbr --add n npm
 abbr --add y yarn
 abbr --add oc opencode
 abbr --add lg lazygit
+
+# mv, rm, cp
+abbr --add mv 'mv -v'
+abbr --add rm 'rm -v'
+abbr --add cp 'cp -v'
+
+abbr --add push "git push"
+abbr --add dels "delta --side-by-side"
+
+# some of my git aliases
+abbr --command=git db diffbranch
+# fully expand because the alias is has a # which fucks with git-filter-branch.
+abbr --command=git dbt "diff origin/main..."
+
+abbr --command=npm i install
+#abbr --command=pnpm i "install"
