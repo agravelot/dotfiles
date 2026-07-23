b40ca9cdaa29fb5ee0faf3588de733537a121da6
Antoine GRAVELOT <antoine.gravelot@hotmail.fr>
2026-07-24 01:31:11 +0200

    hyprland: add window workspace dispatcher

--- hyprland2/.config/hypr/custom/keybinds.lua
+++ hyprland2/.config/hypr/custom/keybinds.lua
@@ -1,1 +1,7 @@
 hl.bind("CTRL+SUPER+ALT+Slash", hl.dsp.exec_cmd("xdg-open ~/.config/hypr/custom/keybinds.lua"), {description = "Edit user keybinds"} )
+
+for i = 1, 10 do
+    hl.bind("SUPER + SHIFT + " .. (i % 10), function()
+        hl.dispatch(hl.dsp.window.move({ workspace = workspace_in_group(i), follow = false }))
+    end, { description = "Window: Send to workspace " .. i })
+end
